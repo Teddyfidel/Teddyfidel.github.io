9a7458004469d36014a82225b6a3f9b030c78f15
Teddy Fidel Valencia Castro <teddyvalencia1997@gmail.com>
2025-02-03 11:24:28 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// scripts.js
+function alquilar() {
+    alert('¡Gracias por elegir nuestro servicio! Redirigiéndote a la página de pago.');
+}
+
+function irCatalogo() {
+    alert('Redirigiéndote al catálogo.');
+}
+
+function conocer() {
+    alert('Descubre más sobre nuestro servicio.');
+}
